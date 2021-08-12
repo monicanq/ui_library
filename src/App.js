a4df21df40ae2f2b468f3288d673ce3cc20eab4f
curlycode <50246670+monicanq@users.noreply.github.com>
2021-08-12 14:17:45 +0100

    added open/close functionality to navbar and styles

--- src/App.js
+++ src/App.js
@@ -4,6 +4,27 @@ function App() {
   return (
     <div className="App">
         <Navbar />
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam aperiam at reiciendis blanditiis modi maiores libero fugiat odit labore, laboriosam tempore mollitia delectus suscipit animi. Veniam odit aut ut obcaecati.</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam aperiam at reiciendis blanditiis modi maiores libero fugiat odit labore, laboriosam tempore mollitia delectus suscipit animi. Veniam odit aut ut obcaecati.</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam aperiam at reiciendis blanditiis modi maiores libero fugiat odit labore, laboriosam tempore mollitia delectus suscipit animi. Veniam odit aut ut obcaecati.</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam aperiam at reiciendis blanditiis modi maiores libero fugiat odit labore, laboriosam tempore mollitia delectus suscipit animi. Veniam odit aut ut obcaecati.</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam aperiam at reiciendis blanditiis modi maiores libero fugiat odit labore, laboriosam tempore mollitia delectus suscipit animi. Veniam odit aut ut obcaecati.</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam aperiam at reiciendis blanditiis modi maiores libero fugiat odit labore, laboriosam tempore mollitia delectus suscipit animi. Veniam odit aut ut obcaecati.</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam aperiam at reiciendis blanditiis modi maiores libero fugiat odit labore, laboriosam tempore mollitia delectus suscipit animi. Veniam odit aut ut obcaecati.</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam aperiam at reiciendis blanditiis modi maiores libero fugiat odit labore, laboriosam tempore mollitia delectus suscipit animi. Veniam odit aut ut obcaecati.</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam aperiam at reiciendis blanditiis modi maiores libero fugiat odit labore, laboriosam tempore mollitia delectus suscipit animi. Veniam odit aut ut obcaecati.</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam aperiam at reiciendis blanditiis modi maiores libero fugiat odit labore, laboriosam tempore mollitia delectus suscipit animi. Veniam odit aut ut obcaecati.</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam aperiam at reiciendis blanditiis modi maiores libero fugiat odit labore, laboriosam tempore mollitia delectus suscipit animi. Veniam odit aut ut obcaecati.</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam aperiam at reiciendis blanditiis modi maiores libero fugiat odit labore, laboriosam tempore mollitia delectus suscipit animi. Veniam odit aut ut obcaecati.</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam aperiam at reiciendis blanditiis modi maiores libero fugiat odit labore, laboriosam tempore mollitia delectus suscipit animi. Veniam odit aut ut obcaecati.</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam aperiam at reiciendis blanditiis modi maiores libero fugiat odit labore, laboriosam tempore mollitia delectus suscipit animi. Veniam odit aut ut obcaecati.</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam aperiam at reiciendis blanditiis modi maiores libero fugiat odit labore, laboriosam tempore mollitia delectus suscipit animi. Veniam odit aut ut obcaecati.</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam aperiam at reiciendis blanditiis modi maiores libero fugiat odit labore, laboriosam tempore mollitia delectus suscipit animi. Veniam odit aut ut obcaecati.</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam aperiam at reiciendis blanditiis modi maiores libero fugiat odit labore, laboriosam tempore mollitia delectus suscipit animi. Veniam odit aut ut obcaecati.</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam aperiam at reiciendis blanditiis modi maiores libero fugiat odit labore, laboriosam tempore mollitia delectus suscipit animi. Veniam odit aut ut obcaecati.</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam aperiam at reiciendis blanditiis modi maiores libero fugiat odit labore, laboriosam tempore mollitia delectus suscipit animi. Veniam odit aut ut obcaecati.</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam aperiam at reiciendis blanditiis modi maiores libero fugiat odit labore, laboriosam tempore mollitia delectus suscipit animi. Veniam odit aut ut obcaecati.</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam aperiam at reiciendis blanditiis modi maiores libero fugiat odit labore, laboriosam tempore mollitia delectus suscipit animi. Veniam odit aut ut obcaecati.</p>
     </div>
   );
 }
--- src/comp/Navbar/Navbar.js
+++ src/comp/Navbar/Navbar.js
@@ -1,39 +1,57 @@
 import { useState } from "react"
-import useIsMobile from "../../hooks/useIsMobile";
+import useIsMobile from "../../hooks/useIsMobile"
 import styles from './navbar.module.css'
-
+import { BrowserRouter as Router,
+        NavLink,
+        Link } from 'react-router-dom'
+ 
 const Navbar = () => {
 
     const [open, setOpen] = useState(false);
     const isMobile = useIsMobile();
-    
+    console.log('render')
     const links = [
-        'About',
-        'Projects',
-        'Contact'
+        {'text': 'About',
+        'url': '/about'},
+        {'text': 'Services',
+        'url': '/services'},
+        {'text': 'Projects',
+        'url': '/projects'},
+        {'text': 'Contact',
+        'url': '/contact'}
     ]
 
     return ( 
-        <nav className={ styles.mainNavbar }>
-            <div className={ styles.logo }>
-                <h1>My Page</h1>
-            </div>
-
-            {(!isMobile || (isMobile & open)) ?
-                <div className={ styles.links }>
-                    { links.map(( link, idx ) => <li key={ idx }> { link } </li>) }
-                </div> 
-                : null
-            }
-
-            {isMobile  && 
-                <div className={ styles.burger } onClick = { open => setOpen( open =>  !open ) }>
-                    <div></div>
-                    <div></div>
-                    <div></div>
+        <Router>
+            <nav className={ !isMobile ? styles.mainNavbar 
+                : open ? `${styles.mainNavbar} ${styles.mobile} ${styles.open}`
+                : `${styles.mainNavbar} ${styles.mobile}` }>
+                <div className={ styles.logo } onClick = { open => setOpen( open =>  !open ) }>
+                    <Link to='/'><h1>My Page</h1></Link>
                 </div>
-            }
-        </nav>
+
+                {(!isMobile || (isMobile & open)) ?
+                        <div className={ !isMobile ? styles.links : `${styles.links} ${styles.mobile}` }>
+                            { links.map(link => 
+                                <li key={ link.text }> 
+                                    <NavLink  to={ link.url } onClick = { open => setOpen( open =>  !open ) }> 
+                                        { link.text } 
+                                    </NavLink> 
+                                </li>) 
+                            }
+                        </div> 
+                    : null
+                }
+
+                {isMobile  && 
+                    <div className={ open ? `${styles.burger} ${styles.active}` : styles.burger  } onClick = { open => setOpen( open =>  !open ) }>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                }
+            </nav>
+        </Router>
      );
 }
  
